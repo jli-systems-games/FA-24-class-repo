e5e139eb164c1c4926c7abcbfeb48ea266fc8cb6
debbie4444 <118382632+debbie4444@users.noreply.github.com>
2024-09-03 12:58:34 -0400

    4

--- Assets/script/Paddle.cs
+++ Assets/script/Paddle.cs
@@ -7,12 +7,13 @@ public class Paddle : MonoBehaviour
     public bool isPlayer1;
     public float speed;
     public Rigidbody2D rb;
+    public Vector3 startPosition;
 
     private float movement;
 
     void Start()
     {
-        
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -28,5 +29,11 @@ public class Paddle : MonoBehaviour
         }
 
         rb.velocity = new Vector2(rb.velocity.x, movement * speed);
+    }
+
+    public void Reset()
+    {
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
     }
 }
