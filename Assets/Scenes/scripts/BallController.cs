12cb0f2fbc2a8b81b510b511fcb6dcb686b92a89
astrvea <mrcchen915@gmail.com>
2024-09-03 05:55:13 -0400

    update

--- Assets/Scenes/scripts/BallController.cs
+++ Assets/Scenes/scripts/BallController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-
     private Vector3 direction;
     public float speed;
+
+    [SerializeField]
+    private int playerOneScore;
+    [SerializeField]
+    private int playerTwoScore;
+
+    public Vector3 spawnPoint;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.direction = new Vector3(1f, 0f, 1f); 
-
+        this.direction = new Vector3(1f, 0f, 1f);
     }
 
     // Update is called once per frame
@@ -21,9 +26,21 @@ public class BallController : MonoBehaviour
         this.transform.position += direction * speed;
     }
 
-    private void OnCollisionEnter(Collision col)
+    private void OnCollisionEnter(Collision collision)
     {
-        Vector3 normal = col.contacts[0].normal;
+        Vector3 normal = collision.contacts[0].normal;
         direction = Vector3.Reflect(direction, normal);
+
+        if (collision.gameObject.name == "WestWall")
+        {
+            playerTwoScore++;
+            transform.position = spawnPoint;
+        }
+
+        if (collision.gameObject.name == "EastWall")
+        {
+            playerOneScore++;
+            transform.position = spawnPoint;
+        }
     }
 }
