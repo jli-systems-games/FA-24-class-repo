4d35357bc6b74d03e968bed5ebca4ace4dbafbce
nanaxliu <nanaliu519@gmail.com>
2024-09-05 06:36:09 -0400

    scoring system finished

--- Assets/scripts/scoreUpdate.cs
+++ Assets/scripts/scoreUpdate.cs
@@ -11,6 +11,8 @@ public class scoreUpdate : MonoBehaviour
     public TMP_Text text3;
     public paddlePoint paddlepoint1;
     public paddlePoint paddlepoint2;
+    public paddlePoint paddlepoint3;
+    public paddlePoint paddlepoint33;
     private string points1;
     private string points2;
     private string points3;
@@ -19,10 +21,11 @@ public class scoreUpdate : MonoBehaviour
     {
         points1 = GameObject.FindWithTag("ppoint1").GetComponent<paddlePoint>().pointScore.ToString();
         points2 = GameObject.FindWithTag("ppoint2").GetComponent<paddlePoint>().pointScore.ToString();
-
+        points3 = ((GameObject.FindWithTag("ppoint3").GetComponent<paddlePoint>().pointScore) + (GameObject.FindWithTag("ppoint33").GetComponent<paddlePoint>().pointScore)).ToString();
 
         text1.text = "P1 points:" + points1;
         text2.text = "P2 points:" + points2;
+        text3.text = "P3 points:" + points3;
     }
 
 }
