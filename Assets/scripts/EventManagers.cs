d9fe0b0394c99bcd2372a1fe1f2ab081be959a19
xshge <143638479+xshge@users.noreply.github.com>
2024-09-07 00:34:32 -0400

    intial commit and eventmanager

---
+++ Assets/scripts/EventManagers.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManagers : MonoBehaviour
+{
+    public float time = 10f;
+    public int fails = 0;
+    public bool gameEnd;
+    IEnumerator manager;
+
+
+    bool firstpass = true;
+    int n = 0;
+    public GameObject canvas1,canvas2,canvas3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = manageEvents(time);
+        StartCoroutine(manager);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(time);
+    }
+    private IEnumerator manageEvents(float t)
+    {  
+        while (!gameEnd)
+        {
+            Debug.Log("gping");
+            determineEvents(); 
+            yield return new WaitForSeconds(t);
+        }
+        
+    }
+
+    void determineEvents()
+    {
+        
+        
+        if(fails <= 2)
+        {
+           
+            if(firstpass)
+            {
+               
+                
+                if(n >= 3)
+                {
+                    firstpass = false;
+                    //return;
+                }else
+                {
+                    n++;
+                }
+            }
+            else
+            {   //ensure that there is not repeating numbers back to back;
+                int nextNumb;
+                do
+                {
+                    nextNumb = Random.Range(1, 4);
+                } while (n == nextNumb);
+
+                n = nextNumb;
+                
+            }
+
+            Debug.Log(n);
+            switch (n)
+                {
+                    case 1:
+                        Event1();
+                        break;
+                    case 2:
+                        Event2();
+                        break;
+                    case 3:
+                        Event3();
+                        break;
+                }
+        }
+        else
+        {
+            //a boolean that will be controlled by the fail counters, which are updated by scripts on event objects.
+            gameEnd = true;
+        }
+    }
+
+    void Event1()
+    {
+        canvas1.SetActive(true);
+
+
+
+        //turn off other events if they are active.
+        if(canvas2.activeSelf)
+        {
+            canvas2.SetActive(false);
+        }else if (canvas3.activeSelf)
+        {
+            canvas3.SetActive(false);
+        }
+
+    }
+    void Event2()
+    {
+        canvas2.SetActive(true);
+
+
+
+        if (canvas1.activeSelf)
+        {
+            canvas1.SetActive(false);
+        }
+        else if (canvas3.activeSelf)
+        {
+            canvas3.SetActive(false);
+        }
+    }
+    void Event3()
+    {
+        canvas3.SetActive(true);
+
+
+
+        if (canvas1.activeSelf)
+        {
+            canvas1.SetActive(false);
+        }
+        else if (canvas2.activeSelf)
+        {
+            canvas2.SetActive(false);
+        }
+    }
+}
