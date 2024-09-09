f75c3086cfcc3ef24f8dbf0302e47437e2aabeaf
mhosster <46171808+HHOOSSPPEE@users.noreply.github.com>
2024-09-09 17:43:14 -0400

    Week03 Demo - Uncommented

---
+++ Assets/03_GameManagers/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    Idle,
+    Game1,
+    Game2, 
+    Game3, 
+    Transition
+}
+public class GameManager : MonoBehaviour
+{
+    public static int health, score;
+    public float time;
+    public static GameState state;
+
+    //Managers
+    public ControllerBonk controllerBonk;
+    public ControllerJump controllerJump;
+
+    public List<GameState> MicroGamePool = new List<GameState>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        //GameObject, gameObject, and this.gameObject
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        state = newState;
+
+        //tell the other scripts that need to know
+        //controllerBonk.ChangeState(state);
+        //controllerJump.ChangeState(state);
+
+        if(state == GameState.Game1)
+        {
+            //controllerBonk.ChangeState(state);
+            controllerBonk.StartMicroGame();
+        }
+    }
+
+    public void ChooseRandomGame()
+    {
+        GameState randomState = MicroGamePool[Random.Range(0,MicroGamePool.Count+1)];
+        ChangeState(randomState);
+    }
+}
