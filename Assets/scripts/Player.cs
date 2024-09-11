859082a7791a729340de311ab9dea5a2f4270138
astrvea <mrcchen915@gmail.com>
2024-09-11 00:25:53 -0400

    scene 1 complete

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public float speed = 3f;
     private Rigidbody2D rb;
     private Vector2 movement;
+
+    public GameObject cat;
+    public TextMeshProUGUI uiText;
+    public string victory = "Victory!";
+    public string failed = "You got caught!";
+    public Collider2D goalCollider;
+
+    private bool isGameOver = false; // to help with the stopmovement function
 
     void Start()
     {
@@ -15,11 +25,50 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        if (isGameOver) return;
+
         movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+        if (!isGameOver)
+        {
+            rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == cat)
+        {
+            // cat wins
+            uiText.text = failed;
+            isGameOver = true;
+            StopMovement();
+        }
+        else if (other == goalCollider)
+        {
+            // mouse wins
+            uiText.text = victory;
+            isGameOver = true;
+            StopMovement();
+        }
+    }
+
+    private void StopMovement()
+    {
+        // stops mouse
+        rb.velocity = Vector2.zero;
+
+        // stops cat
+        if (cat != null)
+        {
+            Animation catAnimation = cat.GetComponent<Animation>();
+            if (catAnimation != null)
+            {
+                catAnimation.Stop();
+            }
+        }
     }
 }
