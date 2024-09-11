a1f687174670adc718dd8673add907358436e8f0
yuouew <tyang2204@gmail.com>
2024-09-11 15:17:15 -0400

    i did literally nothing

--- Assets/Game 1/Game1Script.cs
+++ Assets/Game 1/Game1Script.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Game1Script : MonoBehaviour
 {
+    public bool miniGameWon;
+
+    public Walls;
+
+
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void Game1End()
+    {
+        //should check if player won or loss game??
+        if (miniGameWon == true)
+        {
+
+        } else if (miniGameWon == false)
+        {
+
+        }
+
+        //scoring system?
+        //score++; lmaoo
+
+
+        //reset moving parts positions..
+        transform.localPosition = startPosition;
+        //specify all items?
     }
 }
