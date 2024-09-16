1f7657b94cbb82ce2d2726a79eecc39280e45421
lilyvaughn <143638377+lilyvaughn@users.noreply.github.com>
2024-09-16 17:31:23 -0400

    start screen

---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public float typingSpeed = 0.05f;
+    public float currentTypingSpeed;
+
+    private bool isTyping = false;
+    private bool finishedTyping = false;
+
+    public string[] dialogueLines;
+    private int currentLineIndex = 0;
+
+    private Coroutine typingCoroutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTypingSpeed = typingSpeed;
+        StartNextDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for Enter press
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isTyping)
+            {
+                currentTypingSpeed = typingSpeed / 10;
+            }
+            else if (finishedTyping)
+            {
+                StartNextDialogue();
+            }
+        }
+    }
+
+    void StartNextDialogue()
+    {
+        if (currentLineIndex < dialogueLines.Length)
+        {
+            // Reset the state and start typing the next line
+            finishedTyping = false;
+            currentTypingSpeed = typingSpeed;
+
+            if (typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+            }
+
+            typingCoroutine = StartCoroutine(TypeText(dialogueLines[currentLineIndex]));
+            currentLineIndex++;
+        }
+        else
+        {
+            dialogueText.text = "End of dialogue.";
+        }
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        isTyping = true;
+        dialogueText.text = "";
+
+        foreach (char letter in text.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(currentTypingSpeed);
+        }
+
+        isTyping = false;
+        finishedTyping = true;
+    }
+}
+
