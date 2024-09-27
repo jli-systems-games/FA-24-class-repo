ebd92fc8d5b92c84eae2941db7ebef47f65eba2e
nanaxliu <nanaliu519@gmail.com>
2024-09-26 20:05:02 -0400

    item detection within camera

---
+++ Assets/scripts/detectItem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectItem : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject item;
+    Plane[] cameraFrustum;
+    Collider collider;
+
+    public bool withinTarget;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(withinTarget);
+
+        var bounds = collider.bounds;
+        cameraFrustum = GeometryUtility.CalculateFrustumPlanes(camera);
+        if (GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+        {
+            withinTarget = true;
+        }
+        else
+        {
+            withinTarget = false;
+        }
+    }
+}
