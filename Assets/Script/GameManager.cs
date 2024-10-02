f264a20cbb5b3b5aee57eee35254761d91a7614e
xshge <143638479+xshge@users.noreply.github.com>
2024-10-02 01:28:23 -0400

    UI and Next Levels foundation

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     NavigationManager npcs;
+    public TMP_Text context;
+    public GameObject Panelparent;
+    public string[] lines;
     void Start()
     {
         npcs = FindAnyObjectByType<NavigationManager>();
@@ -19,14 +25,17 @@ public class GameManager : MonoBehaviour
     IEnumerator Instruction()
     {
         //begin dialogue for context
+        context.text = lines[0];
         Debug.Log("Watch out for them cunts that will be busting through door.");
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
 
+        context.text = lines[1];
         Debug.Log("Maybe one of these tiny supes can be of help.");
 
         yield return new WaitForSeconds(2f);
         //Invoke the enemy event
+        Panelparent.SetActive(false);
         npcs.Movement();
     }
 }
