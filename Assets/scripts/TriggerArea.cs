3d928e339466eef7e974e46600aaf3c230b98678
debbie4444 <yuanziqi_yzq@outlook.com>
2024-10-03 11:40:06 -0400

    11

---
+++ Assets/scripts/TriggerArea.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class TriggerArea : MonoBehaviour
+{
+    public GameObject UICanvas; 
+    public GameObject winText;  
+    public GameObject loseText;  
+    private float countdownTime = 10f; 
+    private bool gameEnded = false; 
+    private TextMeshProUGUI timerText; 
+
+    private float timeRemaining;
+
+    void Start()
+    {
+        winText.SetActive(false);
+        loseText.SetActive(false);
+        
+        timerText = UICanvas.GetComponentInChildren<TextMeshProUGUI>();
+        
+        timeRemaining = countdownTime;
+        StartCoroutine(StartCountdown());
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        while (timeRemaining > 0)
+        {
+            timerText.text = $"{(int)(timeRemaining / 60)}:{(int)(timeRemaining % 60):00}";
+            timeRemaining -= Time.deltaTime;
+            yield return null; 
+        }
+
+       
+        if (!gameEnded)
+        {
+            CheckCollisions(false); 
+        }
+    }
+
+    private void CheckCollisions(bool collisionDetected)
+    {
+        if (collisionDetected)
+        {
+            loseText.SetActive(true);
+        }
+        else
+        {
+            winText.SetActive(true);
+        }
+
+        gameEnded = true; 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            CheckCollisions(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (gameEnded)
+        {
+            return; 
+        }
+    }
+}
