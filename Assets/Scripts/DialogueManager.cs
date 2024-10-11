b0110323acadc9fc733b7cb0f18aa66dc917900e
lilyvaughn <143638377+lilyvaughn@users.noreply.github.com>
2024-10-11 14:48:58 -0400

    season select fixed

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -21,8 +21,8 @@ public class DialogueManager : MonoBehaviour
             "Welcome to your color analysis session! Find out which season best suits you!\n\n(This is just for fun. Virtual tools are less accurate than in person color draping.)",
             "The four seasons are differentiated by color temperature (warm or cool), value (light or darkness), and chroma (muted or bright). Your ideal season is determined by your skin, hair, and eye color.\n\nSPRING\nwarm + light  →  bright\nSUMMER\ncool + light  →  muted\nAUTUMN\nwarm + dark  →  muted‍\nWINTER\ncool + dark  →  bright",
             "First, are you warm or cool toned? Please place your face within the oval.\n \nA clashing result can emphasize blemishes and eyebags, or make the skin look green or sickly.",
-            "Are you high or low contrast? Assign features of your face (skin, hair, eyes, and mouth) a value of 1, 2, or 3 for light, medium, or dark.\n\n\n\n\n\n\n\n\n\n "
-            //"Please select your undertone and contrast level."
+            "Are you high or low contrast? Assign features of your face (skin, hair, eyes, and mouth) a value of 1, 2, or 3 for light, medium, or dark.\n\n\n\n\n\n\n\n\n\n ",
+            "Please select your undertone and contrast level."
         };
 
         uiElementDisplay = new List<List<GameObject>>()
@@ -30,8 +30,8 @@ public class DialogueManager : MonoBehaviour
             new List<GameObject> { GameObject.Find("next button") },
             new List<GameObject> { GameObject.Find("next button"), GameObject.Find("seasonal palettes") },
             new List<GameObject> { GameObject.Find("next button"), GameObject.Find("undertone"), GameObject.Find("webcam image") },
-            new List<GameObject> { GameObject.Find("next button"), GameObject.Find("contrast") }
-            //new List<GameObject> { GameObject.Find("selection"), GameObject.Find("season select") }
+            new List<GameObject> { GameObject.Find("next button"), GameObject.Find("contrast"), GameObject.Find("contrast webcam") },
+            new List<GameObject> { GameObject.Find("selection") },
         };
 
         UpdateUI();
