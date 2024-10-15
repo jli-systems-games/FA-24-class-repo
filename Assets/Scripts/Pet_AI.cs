cb77363764212f7c918b3b01ba86584a5adf98ff
toastyghostii <kikithecoolcookie@gmail.com>
2024-10-15 18:10:36 -0400

    coded pet movement

--- Assets/Scripts/Pet_AI.cs
+++ Assets/Scripts/Pet_AI.cs
@@ -16,13 +16,19 @@ public class Pet_AI : MonoBehaviour
 
     public bool asleep;
 
-    public Transform mouseLoc;
+    public GameObject mouseLoc;
+    private Vector2 petToMouseVector;
+    private Vector2 directionToMouse;
+
+    private Rigidbody2D rb;
 
     public float speed;
+    public float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
 
         stomache = 10;
         criticalHit = 1;
@@ -42,7 +48,37 @@ public class Pet_AI : MonoBehaviour
         }
 
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseLoc.transform.position = new Vector3(mousePos.x,mousePos.y,0);
+        petToMouseVector = mouseLoc.transform.position - transform.position;
+        directionToMouse = petToMouseVector.normalized;
 
+        Debug.Log(directionToMouse);
+    }
+
+    private void FixedUpdate()
+    {
+        SetVelocity();
+        RotateTowardsTarget();
+    }
+
+    void RotateTowardsTarget()
+    {
+        Vector2 current = transform.forward;
+
+        transform.up = Vector3.RotateTowards(current, petToMouseVector, 20, rotationSpeed * Time.deltaTime);
+    }
+
+    void SetVelocity()
+    {
+        if(directionToMouse == Vector2.zero)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        else
+        {
+            rb.velocity = transform.up * speed;
+        }
     }
 
     public void calculateDamage()
