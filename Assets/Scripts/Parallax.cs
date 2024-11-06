14c2373b89a0b95de3440786c2f87b75fd93b8bf
frida laurvigen <148938747+fridalaurvigenn@users.noreply.github.com>
2024-11-06 05:15:03 -0500

    Adding annotations to parallax script and changed around game scene

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,33 +4,36 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    public float depth = 1;  // Determines the parallax effect strength; higher values move slower
 
-    public float depth = 1;
-
-    Player player; // Reference to the Player script, used to access player data 
+    Player player; // Reference to the Player script to access player velocity
 
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        // Calculate the parallax effect by adjusting the player’s X velocity based on depth
         float realVelocity = player.velocity.x / depth;
+
+        // Get the current position of the parallax object
         Vector2 pos = transform.position;
 
+        // Move the background position to the left based on the calculated velocity
         pos.x -= realVelocity * Time.fixedDeltaTime;
 
+        // Loop the background position if it moves too far left, creating a seamless effect
         if (pos.x <= -20)
             pos.x = 20;
 
+        // Update the position of the object to create the parallax effect
         transform.position = pos;
     }
 }
