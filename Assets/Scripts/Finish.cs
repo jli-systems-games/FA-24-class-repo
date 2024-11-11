f62e612d66c73ff9561bc621137a133818128f94
Olivy815 <145684853+Olivy815@users.noreply.github.com>
2024-11-11 16:18:31 -0500

    game

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+
---
+++ Assets/Scripts/SectionTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionTrigger : MonoBehaviour
+{
+    public GameObject roadSection;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            Instantiate(roadSection, new Vector3(0, 0, 26), Quaternion.identity);
+        }
+    }
+
+}
