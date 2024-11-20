49805236c8a0e93a9e03262e3cb22d8402b82551
toastyghostii <kikithecoolcookie@gmail.com>
2024-11-20 18:57:45 -0500

    aaaaaaaaaaaaaaaaaaaa

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    moveable,
+    matchOut,
+    burning,
+    dead
+}
+public class GameManager : MonoBehaviour
+{
+    public static GameState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
