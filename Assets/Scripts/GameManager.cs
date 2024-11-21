33f038434fcacf5f20f16b09bdb105e309c8b834
toastyghostii <kikithecoolcookie@gmail.com>
2024-11-21 00:10:54 -0500

    light match mech added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,15 +14,27 @@ public class GameManager : MonoBehaviour
 {
     public static GameState state;
 
+    public GameObject match;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        match.SetActive(false);
+        state = GameState.moveable;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1) && state == GameState.moveable)
+        {
+            match.SetActive(true);
+            ChangeState(GameState.matchOut);
+        }
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        state = newState;
     }
 }
--- Assets/Scripts/LightMatch.cs
+++ Assets/Scripts/LightMatch.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class LightMatch : MonoBehaviour
 {
+    private GameManager _gameManager;
+    public GameObject fire;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gameManager = FindObjectOfType<GameManager>();
+        fire.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Mouse X") > 5 || Input.GetAxis("Mouse X") < -5) { 
+            fire.SetActive(true); 
+           _gameManager.ChangeState(GameState.burning); }
         Debug.Log(Input.GetAxis("Mouse X"));
     }
 }
