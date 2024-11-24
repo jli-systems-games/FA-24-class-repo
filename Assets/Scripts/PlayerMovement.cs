2c1d3d349b69b1f5bca2bb3473e061a8d30b5dac
toastyghostii <kikithecoolcookie@gmail.com>
2024-11-24 15:23:54 -0500

    changed rotation, set up base for new movement system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerDirection //the direction the player is facing
+{
+    North,
+    East,
+    South,
+    West
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject[] tiles;
     private GameManager _gameManager;
     public Camera mainCamera;
     private Vector3 mousePos;
 
     private Ray ray;
     private RaycastHit hit;
+
+    private GameObject forwardTile;
+    private GameObject backTile;
+    private GameObject leftTile;
+    private GameObject rightTile;
+
+    public PlayerDirection facing;
 
     // Start is called before the first frame update
     void Start()
@@ -21,37 +35,75 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        //ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-        mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (Input.GetMouseButtonDown(0) && GameManager.state == GameState.moveable)
-            {
-                if (hit.collider.tag == "ground")
-                {
-                    foreach(GameObject tile in tiles)
-                    {
-                        if (hit.collider.gameObject == tile)
-                        {
-                            transform.position = new Vector3(tile.transform.position.x, 1.148575f, tile.transform.position.z);
-                        }
-                    }
-                    Debug.Log("hit tiles");
-                    Debug.Log(hit.point);
-                }
-            }
-        }
+        //if (Physics.Raycast(ray, out hit))
+        //{
+        //    if (Input.GetMouseButtonDown(0) && GameManager.state == GameState.moveable)
+        //    {
+        //        if (hit.collider.tag == "ground")
+        //        {
+        //            foreach(GameObject tile in tiles)
+        //            {
+        //                if (hit.collider.gameObject == tile)
+        //                {
+        //                    transform.position = new Vector3(tile.transform.position.x, 1.148575f, tile.transform.position.z);
+        //                }
+        //            }
+        //            Debug.Log("hit tiles");
+        //            Debug.Log(hit.point);
+        //        }
+        //    }
+        //}
     }
 
-    public void RotateLeft()
+    void SetTiles()
     {
-        transform.Rotate(0, -90,0);
+        //set the tiles somewhere here relative to the player directions & call this every time they move or rotate
+    }
+    #region Movement
+    public void MoveForward()
+    {
+
+    }
+    public void MoveBack()
+    {
+
+    }
+    public void MoveLeft()
+    {
     }
 
-    public void RotateRight()
+    public void MoveRight()
     {
-        transform.Rotate(0, 90, 0);
     }
+    #endregion
+
+    #region Rotations
+    public void RotateNorth()
+    {
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        facing = PlayerDirection.North;
+    }
+
+    public void RotateSouth()
+    {
+        transform.eulerAngles = new Vector3(0, 180, 0);
+        facing = PlayerDirection.South;
+    }
+    public void RotateWest()
+    {
+        transform.eulerAngles = new Vector3(0,270,0);
+        facing = PlayerDirection.West;
+    }
+
+    public void RotateEast()
+    {
+        transform.eulerAngles = new Vector3(0, 90, 0);
+        facing = PlayerDirection.East;
+    }
+    #endregion
+
 }
---
+++ Assets/Scripts/TileDirections.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileDirections : MonoBehaviour
+{
+    public GameObject northTile;
+    public GameObject southTile;
+    public GameObject eastTile;
+    public GameObject westTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("north"))
+        {
+            other.gameObject.transform.parent.GetComponent<TileDirections>().northTile = this.gameObject;
+        }
+        else if (other.CompareTag("south"))
+        {
+            other.gameObject.transform.parent.GetComponent<TileDirections>().southTile = this.gameObject;
+        }
+        else if (other.CompareTag("east"))
+        {
+            other.gameObject.transform.parent.GetComponent<TileDirections>().eastTile = this.gameObject;
+        }
+        else if (other.CompareTag("west"))
+        {
+            other.gameObject.transform.parent.GetComponent<TileDirections>().westTile = this.gameObject;
+        }
+    }
+}
