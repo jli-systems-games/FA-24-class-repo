ef219000235bf84572d59d422d1731db85fe7c97
xshge <143638479+xshge@users.noreply.github.com>
2024-12-18 13:35:43 -0500

    fixes and stuff
    I don't remember the last fix was

--- Assets/Scripts/DrawLine.cs
+++ Assets/Scripts/DrawLine.cs
@@ -95,6 +95,7 @@ public class DrawLine : MonoBehaviour
                 lastDirct.Normalize();
                  
                 prevDirct = lastDirct;
+                
                  
              }
             else
